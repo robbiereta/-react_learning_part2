d6744b66b9dd7e6c70973c362a2a5ebccc4b6153
Robbie Reta <robbie.reta67@gmail.com>
2017-07-16 16:26:35 -0500

    Writing More Reusable React Components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,76 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+  	super();
+  	this.state = {
+      red:0,
+    };
+    this.update=this.update.bind(this)
+  }
+  update(e){
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
 
+    })
+  }
   render() {
     return (
-			<Buttons>
-        <button value="A">A</button>
-        <button value="B">B</button>
-        <button value="C">C</button>
-      </Buttons>
+			<div>
+        <NumInput
+          ref="red"
+          update={this.update}
+          min={1}
+          max={255}
+          step={1}
+          val={+this.state.red}
+          label="Red"
+          update={this.update}/>
+			</div>
     );
   }
 }
 
-class Buttons extends React.Component {
-  constructor(){
-    super();
-    this.state = {selected: 'None'}
-  }
-  selectItem(selected){
-    this.setState({selected})
-  }
-    // para tab interface,nav component,radio group
+class NumInput extends React.Component {
+
     render() {
-      let fn = child =>
-        React.cloneElement(child, {
-        onClick: this.selectItem.bind(this, child.props.value)
-      })
-      let items= React.Children.map(this.props.children, fn);
+      let label = this.props.label !== '' ? //si no existe label entonces se crea una , sino es un empty string
+        <label>{this.props.label} - {this.props.val}</label> : ''
         return (
             <div>
-              <h2> You have Selected {this.state.selected}</h2>
-              {items}
+            <input ref="inp"
+              type={this.props.type}
+              min={this.props.min}
+              max={this.props.max}
+              step={this.props.step}
+              defaultValue={this.props.val}
+              onChange={this.props.update}/>
+              {label}
+
             </div>
 
         )
     }
 }
 
+NumInput.propTypes={
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
+  val: PropTypes.number,
+  label: PropTypes.string,
+  update: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['number','range'])
+}
+NumInput.defaultProps = {
+  min:0,
+  max:0,
+  step:1,
+  val:0,
+  label:'',
+  type:'range'
+}
 export default App;
